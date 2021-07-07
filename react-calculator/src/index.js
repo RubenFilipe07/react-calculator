89844005c5e5c16df604da2cb215d7798e10470f
Rúben Filipe <rubenfilipe.net@gmail.com>
2021-07-07 03:36:57 -0300

    sending files

---
+++ react-calculator/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './main/Calculator';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <div>
+    <Calculator/>
+  </div>, document.getElementById('root'));
+reportWebVitals();
